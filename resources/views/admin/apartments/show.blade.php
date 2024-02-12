700d0bb8ed7d6cbffffb10a22ad13c8f9ff79c4e
RickCraco <riccardocracolici43@gmail.com>
2024-02-12 18:07:02 +0100

    fix

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -11,8 +11,5 @@
         </div>
 
         <h4>Payment</h4>
-
-        <div id="dropin-container"></div>
-        <button id="submit-button" class="button button--small button--green">Purchase</button>
     </section>
 @endsection
