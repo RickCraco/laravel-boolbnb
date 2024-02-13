72b08cc85f37e4126c9f3a21c912b47c7e650a1e
RickCraco <riccardocracolici43@gmail.com>
2024-02-13 18:03:19 +0100

    added services api

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\ServiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,5 @@ Route::get('/apartments', [ApartmentController::class, 'index']);
 Route::get('apartments/{apartment:slug}', [ApartmentController::class, 'show']);
 Route::post('apartments/{apartment:slug}/views', [ApartmentController::class, 'recordView']);
 Route::get('search/apartments', [ApartmentController::class, 'search']);
+
+Route::get('/services', [ServiceController::class, 'index']);
