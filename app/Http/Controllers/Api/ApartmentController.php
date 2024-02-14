9c4ba832691297591cc47ddd7835d0375b401764
RickCraco <riccardocracolici43@gmail.com>
2024-02-14 12:22:06 +0100

    added api route for messages

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
 
 class ApartmentController extends Controller
 {
@@ -48,5 +49,24 @@ class ApartmentController extends Controller
         }
 
         return response()->json(['message' => 'Visualizzazione registrate con successo'], 200); 
+    }
+
+    public function recordMessage(Apartment $apartment, Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'surname' => 'required',
+            'phone_number' => 'required',
+            'email' => 'required|email',
+            'body' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $apartment->messages()->create(['name' => $request->name, 'surname' => $request->surname, 'phone_number' => $request->phone_number, 'email' => $request->email, 'body' => $request->body, 'apartment_id' => $apartment->id]);
+        return response()->json(['message' => 'Messaggio inviato con successo'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/apartments', [ApartmentController::class, 'index']);
 Route::get('apartments/{apartment:slug}', [ApartmentController::class, 'show']);
 Route::post('apartments/{apartment:slug}/views', [ApartmentController::class, 'recordView']);
+Route::post('apartments/{apartment:slug}/message', [ApartmentController::class, 'recordMessage']);
 Route::get('search/apartments', [ApartmentController::class, 'search']);
 
 Route::get('/services', [ServiceController::class, 'index']);
