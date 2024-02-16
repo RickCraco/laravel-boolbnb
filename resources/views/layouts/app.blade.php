ff8f211061195cac1e39b1f2f18a2e8477af328b
alexraduca01 <alex.raduca01@icloud.com>
2024-02-16 10:41:19 +0100

    added an anchor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
 
         <nav class="navbar navbar-expand-md navbar-light shadow-sm bg-dark">
             <div class="container">
-                
+
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -36,6 +36,9 @@
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
                             <a class="nav-link text-white" href="{{url('/') }}">{{ __('Home') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="http://localhost:5174">{{ __('BoolBnB') }}</a>
                         </li>
                     </ul>
 
