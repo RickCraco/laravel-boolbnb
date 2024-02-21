f7bbb64d2aacf41a2785e6af22f758e3ba85778f
alexraduca01 <alex.raduca01@icloud.com>
2024-02-21 10:11:30 +0100

    edited messages show

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.app')
 @section('content')
 
-<h2 class="text-white">
-    {{ $message->name }}
-</h2>
+<div class="container my-5 pt-5">
+    <h2 class="text-center pb-2 text-white">{{ $message->name }} {{ $message->surname }}</h2>
+    <div class="row d-flex justify-content-center">
+        <div class="card px-0">
+            <div class="card-header">
+              <h3 class="ms-3">{{ $message->email }}</h3>
+            </div>
+            <ul class="list-group list-group-flush">
+                @if($message->phone_number)
+                    <li class="list-group-item fs-5 fw-bold ms-3">Phone number: <br> <span class="fw-normal">{{ $message->phone_number }}</span></li>
+                @endif
+                <li class="list-group-item fs-5 fw-bold ms-3">Message: <br> <span class="fw-normal">{{ $message->body }}</span></li>
+            </ul>
+          </div>
+    </div>
+
+</div>
 
 @endsection
