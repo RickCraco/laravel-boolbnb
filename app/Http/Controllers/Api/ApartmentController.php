6e05ccf7786c888d1fd9868363a77a71906f65ce
RickCraco <riccardocracolici43@gmail.com>
2024-02-21 11:32:50 +0100

    altra prova

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -99,6 +99,7 @@ class ApartmentController extends Controller
 
         $apartments->leftJoin('apartment_sponsor', 'apartments.id', '=', 'apartment_sponsor.apartment_id')
         ->select('apartments.*')
+        ->where('apartments.visible', '=', 1) // Aggiungi questa riga
         ->groupBy('apartments.id')
         ->orderByRaw('CASE WHEN COUNT(apartment_sponsor.sponsor_id) > 0 THEN 0 ELSE 1 END');
 
