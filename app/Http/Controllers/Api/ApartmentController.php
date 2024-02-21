ca52c10d042ef5ca73e3884699ea42ca628c19be
RickCraco <riccardocracolici43@gmail.com>
2024-02-21 11:02:14 +0100

    prova ordinamento sponsor nella ricerca

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -96,6 +96,12 @@ class ApartmentController extends Controller
         // Filtra solo gli appartamenti visibili
         $apartments->where('visible', '=', 1);
 
+        $apartments->leftJoin('apartment_sponsor', 'apartments.id', '=', 'apartment_sponsor.apartment_id')
+        ->select('apartments.*')
+        ->groupBy('apartments.id')
+        ->orderByRaw('CASE WHEN COUNT(apartment_sponsor.sponsor_id) > 0 THEN 0 ELSE 1 END')
+        ->get();
+
         // Esegui la query e restituisci i risultati
         $filteredApartments = $apartments->get();
 
