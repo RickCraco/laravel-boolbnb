d16d29d16dfdd3c4008c64463f068c56849f4c03
federicomanni97 <manni_federico@libero.it>
2024-02-23 13:23:36 +0100

    show messages updated

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="container my-5 pt-5">
-    <h2 class="text-center pb-2 text-white">{{ $message->name }} {{ $message->surname }}</h2>
+    {{-- <h2 class="text-center pb-2 text-white">{{ $message->name }} {{ $message->surname }}</h2>
     <div class="row d-flex justify-content-center">
         <div class="card px-0">
             <div class="card-header">
@@ -15,7 +15,89 @@
                 <li class="list-group-item fs-5 fw-bold ms-3">Message: <br> <span class="fw-normal">{{ $message->body }}</span></li>
             </ul>
           </div>
-    </div>
+    </div> --}}
+    <center class="wrapper">
+        <table class="top-panel center" width="602" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+            <tr>
+                <td class="title text-white fs-2" width="300">BoolBnB</td>
+                <td class="subject" width="300"><a class="strong text-white" href="#" target="_blank">www.BoolBnB.com</a></td>
+            </tr>
+            <tr>
+                <td class="border" colspan="2">&nbsp;</td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div class="spacer">&nbsp;</div>
+
+        <table class="main center" width="602" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+            <tr>
+                <td class="column">
+                    <div class="column-top">&nbsp;</div>
+                    <table class="content" border="0" cellspacing="0" cellpadding="0">
+                        <tbody>
+                        <tr>
+                            <td class="padded">
+                              <h1>Email Received From:</h1>
+
+                              <table style="width:100%">
+                              <tr>
+                                <td><strong>Name</strong></td>
+                                <td>{{ $message->name }}</td>
+                              </tr>
+                              <tr>
+                                <td><strong>Surname</strong></td>
+                                <td>{{ $message->surname }}</td>
+                              </tr>
+                              @if ($message->phone_number)
+                                <tr>
+                                    <td><strong>Phone Number<strong></td>
+                                    <td>{{ $message->phone_number }}</td>
+                                </tr>
+                              @endif
+                              <tr>
+                                <td><strong>Email:</strong></td>
+                                <td>{{ $message->email }}</td>
+                              </tr>
+                              <tr>
+                                <td><strong>Received At:</strong></td>
+                                <td>{{ $message->created_at->format('d/m/Y H:i') }}</td>
+                              </tr>
+                            </table><br>
+                            <p>Message:</p>
+                              {{-- <p class="caption fs-6">{{ $message->body }}</p> --}}
+                              <p class="caption fs-6">Cari interessati,
+                                Vi scrivo per segnalarvi un'appartamento incredibile appena disponibile per l'affitto. Questo gioiello presenta una combinazione perfetta di comfort moderno e design accattivante. Situato in una zona tranquilla ma comodamente collegata, offre spazi luminosi, una cucina completamente attrezzata e una vista mozzafiato. Non perdete l'opportunità di chiamarlo casa vostra!
+                                Per maggiori dettagli e per organizzare una visita, non esitate a contattarmi al più presto. Questa potrebbe essere l'occasione che stavate cercando per trovare il vostro rifugio ideale. Attendiamo con impazienza di accompagnarvi alla scoperta di questo fantastico appartamento.</p>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="column-bottom">&nbsp;</div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+        <div class="spacer">&nbsp;</div>
+
+        <table class="footer center" width="602" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+            <tr>
+                <td class="border" colspan="2">&nbsp;</td>
+            </tr>
+            <tr class="">
+                <td class="subscription" width="300">
+                    <div class="logo-image">
+                        <img src="/public/img/logo-alt.png" alt="logo-alt" width="70" height="70">
+                    </div>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </center>
 
 </div>
 
